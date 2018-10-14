ec55a27071d11f5566913c1c990755b115701b6c
Exodarkens <38673513+Exodarkens@users.noreply.github.com>
2018-10-13 17:41:21 -0700

    Add files via upload

---
+++ L2/l2_12_1050 DDD.pas
@@ -0,0 +1,33 @@
+Program l2_12_1050 ;
+var
+	ddd,cadastrados: integer;
+ Begin
+ 	readln(ddd);
+ 	if(ddd =61 )then
+ 		writeln('Brasilia')
+	else
+		if(ddd =71)then
+			writeln('Salvador')
+		else
+		if(ddd =11)then
+			writeln('Sao paulo')
+		else
+			if(ddd =21)then
+				writeln('Rio de janeiro')
+			else
+				if(ddd =32)then
+					writeln('Juiz de fora')
+				else
+				if(ddd =19)then
+					writeln('Campinas')
+				else
+				if(ddd =27)then
+					writeln('Vitoria')
+				else
+					if(ddd =31)then
+						writeln('Belo Horizonte')
+					else
+						writeln('DDD nao cadastrado');			
+				
+
+ End.
